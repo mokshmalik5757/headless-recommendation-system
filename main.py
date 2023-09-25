0f5889964e3924009d64cba195f2a6484d3fb56e
Moksh Malik <mokshmalik5757@gmail.com>
2023-09-25 15:14:13 +0530

    Modify main.py, changed CORS

--- main.py
+++ main.py
@@ -88,11 +88,11 @@ recommended_products = {
 
 app = FastAPI(title="Recommendation system API", debug = True)
 
-origins = ['*']
+# origins = ['*']
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origin_regex=r"https?://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
